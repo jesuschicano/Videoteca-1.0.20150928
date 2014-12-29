cdadeadba7f02501b61a20ecadd0290b7098ca1e
jesus <jesuschicano3988@gmail.com>
2014-12-30 00:16:36 +0100

    Update 1.0.2914

---
+++ conexion.php
@@ -0,0 +1,21 @@
+<?php
+/*
+@author: Jesús Chicano Ramírez
+@version: 1.0
+@date: 04/09/2014
+*/
+
+// función que se conecta a la base de datos
+//
+function conectaDb(){
+	$dbname = 'videoteca';
+	$user = 'root';
+	$pass = 'chiwy';
+
+	try{
+		$db = new PDO("mysql:host=localhost;dbname=".$dbname, $user, $pass);
+		return($db);
+	}catch(PDOException $e){
+		echo "No se ha podido conectar a la BD";
+	}
+}
